59a4604d32e94b7ac83d3f76dab310bf6a0a49b9
yuxiang <yuxiangli.0908@gmail.com>
2026-02-18 23:38:37 -0500

    add router

--- app/api/router.py
+++ app/api/router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from app.api import heartbeat, rating, shipment
+from app.api import heartbeat, rating, shipment, label
 
 api_router = APIRouter()
 api_router.include_router(heartbeat.router, tags=["health"])
 api_router.include_router(rating.router, tags=["rating"])
 api_router.include_router(shipment.router, tags=["shipment"])
+api_router.include_router(label.router, tags=["label"])
