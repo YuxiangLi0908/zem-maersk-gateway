62e406af521b6e81e714410b2d8609c5972da786
yuxiang <yuxiangli.0908@gmail.com>
2026-03-11 08:24:05 -1000

    move to production

--- app/services/config.py
+++ app/services/config.py
@@ -16,21 +16,25 @@ class AppConfig:
         self.CONTROL_STATION = "GOP"
         self.PAY_TYPE = "ThirdParty"
 
-        self.ACCESS_TOKEN_URL = "https://api-stage.maersk.com/oauth2/access_token?grant_type=client_credentials&client_id={client_id}&client_secret={client_secret}".format(
+        self.ACCESS_TOKEN_URL = "https://api.maersk.com/oauth2/access_token?grant_type=client_credentials&client_id={client_id}&client_secret={client_secret}".format(
             client_id=self.CLIENT_ID, client_secret=self.CLIENT_SECRET
         )
-        self.RATE_API_URL = "https://wsi.pilotdelivers.com/pilotapi/test/v1/Ratings"
+        self.RATE_API_URL = "https://wsi.pilotdelivers.com/pilotapi/v1/Ratings"
         self.SHIPMENT_API_URL = (
-            "https://api-stage.maersk.com/mgf/public-gateway/shipments"
+            # "https://api-stage.maersk.com/mgf/public-gateway/shipments"
+            "https://api.maersk.com/mgf/public-gateway/shipments"
         )
         self.SHIPMENT_VOID_API_URL = (
-            f"https://wsi.pilotdelivers.com/pilotapi/test/v1/Shipments/Void"
+            # f"https://wsi.pilotdelivers.com/pilotapi/test/v1/Shipments/Void"
+            "https://wsi.pilotdelivers.com/pilotapi/v1/Shipments/Void"
         )
         self.LABEL_API_URL = (
-            "https://pilotws.pilotdelivers.com/copilotforms_dev/wsforms.asmx"
+            # "https://pilotws.pilotdelivers.com/copilotforms_dev/wsforms.asmx"
+            "https://pilotws.pilotdelivers.com/copilotforms/wsforms.asmx"
         )
         self.TRACKING_API_URL = (
-            "https://wsi.pilotdelivers.com/test/pilotpartnertracking.asmx"
+            # "https://wsi.pilotdelivers.com/test/pilotpartnertracking.asmx"
+            "https://wsi.pilotdelivers.com/pilotpartnertracking.asmx"
         )
         self.THIRD_PARTY = {
             "address": {
